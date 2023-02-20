1afc17a23a94938b26e8a90d1423b26b00f3d13f
benben <benben@benben.com>
2023-02-20 18:01:30 +0800

    增加nav结构，优化content结构

---
+++ src/api/jj-nav/controllers/jj-nav.ts
@@ -0,0 +1,7 @@
+/**
+ * jj-nav controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::jj-nav.jj-nav');
---
+++ src/api/jj-nav/routes/jj-nav.ts
@@ -0,0 +1,7 @@
+/**
+ * jj-nav router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::jj-nav.jj-nav');
