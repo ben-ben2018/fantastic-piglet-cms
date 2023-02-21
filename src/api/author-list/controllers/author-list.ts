fabf11b69c9f0172642ff0ee6b2c1147dec20757
benben <benben@benben.com>
2023-02-21 10:28:42 +0800

    优化作者榜结构

---
+++ src/api/author-list/controllers/author-list.ts
@@ -0,0 +1,7 @@
+/**
+ * author-list controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::author-list.author-list');
---
+++ src/api/author-list/services/author-list.ts
@@ -0,0 +1,7 @@
+/**
+ * author-list service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::author-list.author-list');
